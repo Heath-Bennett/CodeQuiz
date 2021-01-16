04b1f363279bb90488a82686ebcfc5c58f373fe0
Heath Bennett <heathben418@gmail.com>
2021-01-16 00:37:21 -0500

    working on go back butotn

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,8 +1,11 @@
 //Declare variables
-
-let timerCount = 75;
+let timerCount = 60;
 let answerKey = "";
-let arrayLength = 10;
+let arrayLength = 12;
+let didWin = false;
+let numCorrect = 0;
+let answeredQuestions = 0; 
+let initial = "";
 const timeLeft = $("#timeLeft");
 const qText = $("h1");
 const optOne = $("#qOne");
@@ -12,11 +15,19 @@ const optFour = $("#qFour");
 const mainP = $("main > p");
 const result = $("h3");
 const startBtn = $("#start");
-const ansBtn = $("#form.answer-btns");
-const highScores = $("form.highScores");
+const ansBtn = $("form.answer-btns");
+const highScores = $(".highScores");
+const userInitials = $("form.enter-initial");
+const initialBtn = $("button.initial-button");
+const initialTxt = $("#initials");
+const firstPlace = $("#first");
+const secondPlace = $("#second");
+const thirdPlace = $("#third");
+const fourthPlace = $("#fourth");
+const fifthPlace = $("#fifth");
+const backButton = $("#goBack");
 
 //create an array of question objects
-
 const quizQuestions = [
     {
         question: "What is the proper syntax to create an empty array?", 
@@ -106,6 +117,24 @@ const quizQuestions = [
         optionThree: "Math.max(2, 4)", 
         optionFour: "ceil(2, 4)", 
         answer: "3"
+    }, 
+
+    {
+        question: "In JavaScript, the following loop will execute how many times? for (let x=1; i<11; i++);", 
+        optionOne: "Nine", 
+        optionTwo: "Ten", 
+        optionThree: "Eleven", 
+        optionFour: "It won't loop. Wrong Syntax", 
+        answer: "2"
+    }, 
+
+    {
+        question: "In JavaScript, the symbols + - * and / are:", 
+        optionOne: "operators", 
+        optionTwo: "expressions", 
+        optionThree: "comparison operators", 
+        optionFour: "None of the above", 
+        answer: "1"
     }
 
 ];
@@ -113,6 +142,9 @@ const quizQuestions = [
 //An array to store already used questions
 
 let questionsUsed = [];
+
+// An array to store high scores as objects. 
+let topScores = [];
 
 // creates link for correct answer and wrong answer sound affects. 
 let correctAns = document.createElement("audio");
@@ -126,24 +158,31 @@ wrongAns.setAttribute("src", "./Assets/Sounds/buzz2.mp3");
 //JQuery Document.ready function
 $(document).ready(function() {
 
-    // startTime function runs the timer
+    // startTime function runs the timer 
     function startTime() {
         timer = setInterval(function() {
             timerCount--;
             timeLeft.text(timerCount);
-            if (timerCount === 0){
+            if (timerCount > 0) {
+                checkWin();
+                if(didWin){
+                    clearInterval(timer);
+                    youWon();
+                }
+            }
+            if (timerCount <= 0){
                 clearInterval(timer);
+                
             }
         }, 1000)
     }
     
     //This generates a question object from quizQuestions array and populates the page with it.
     function getQuestion (){
-        let index = Math.floor(Math.random()*arrayLength)
+        let index = Math.floor(Math.random()*arrayLength);
         let currentQuestion = quizQuestions[index];
         let removeQuestion = quizQuestions.splice(index, 1);
         arrayLength--;
-        
         questionsUsed.push(removeQuestion);
         qText.text(currentQuestion.question);
         optOne.text(currentQuestion.optionOne);
@@ -151,16 +190,166 @@ $(document).ready(function() {
         optThree.text(currentQuestion.optionThree);
         optFour.text(currentQuestion.optionFour);
         answerKey = currentQuestion.answer;
-
-        //****************************For testing purposes *****************************/
-        console.log(removeQuestion);
-        console.log(quizQuestions.length);
-        console.log("quiz question length: " + questionsUsed.length);
-        for (let i = 0; i < questionsUsed.length; i++){
-            console.log(questionsUsed[i]);
-        }
-        //***************************************************************************** */
     }
+
+    //This function checks to see if criteria for win has been met
+
+    let checkWin = function (){
+        if (numCorrect >= 5 && answeredQuestions === 10){
+            didWin = true;
+            return didWin;
+        }
+    }
+
+    //This function prompts for user initials and sets high score
+    let getInitials = function(){
+        userInitials.removeClass("display");
+        initialBtn.on("click", function(){
+            let initialArray = initialTxt.val();
+            initial = initialArray.toString();
+            setHighScore(validInitials(initial), timerCount);
+            highScores.removeClass("display");
+            userInitials.addClass("display");
+            //********************************************testing purposes************* */
+            console.log(initial);
+            // $("#first").text(topScores[0].initial + ": " + topScores[0].time + " seconds");
+            //******* *************************************************************** */
+
+            
+            switch(topScores.length){
+                case 5:
+                    firstPlace.text(topScores[0].initial + ": " + topScores[0].time + " seconds");
+                    secondPlace.text(topScores[1].initial + ": " + topScores[1].time + " seconds");
+                    thirdPlace.text(topScores[2].initial + ": " + topScores[2].time + " seconds");
+                    fourthPlace.text(topScores[3].initial + ": " + topScores[3].time + " seconds");
+                    fifthPlace.text(topScores[4].initial + ": " + topScores[4].time + " seconds");
+                    break;
+                case 4:
+                    firstPlace.text(topScores[0].initial + ": " + topScores[0].time + " seconds");
+                    secondPlace.text(topScores[1].initial + ": " + topScores[1].time + " seconds");
+                    thirdPlace.text(topScores[2].initial + ": " + topScores[2].time + " seconds");
+                    fourthPlace.text(topScores[3].initial + ": " + topScores[3].time + " seconds");
+                    break;
+                case 3:
+                    firstPlace.text(topScores[0].initial + ": " + topScores[0].time + " seconds");
+                    secondPlace.text(topScores[1].initial + ": " + topScores[1].time + " seconds");
+                    thirdPlace.text(topScores[2].initial + ": " + topScores[2].time + " seconds");
+                    break;
+                case 2:
+                    firstPlace.text(topScores[0].initial + ": " + topScores[0].time + " seconds");
+                    secondPlace.text(topScores[1].initial + ": " + topScores[1].time + " seconds");
+                    break;
+                case 1:
+                    firstPlace.text(topScores[0].initial + ": " + topScores[0].time + " seconds");
+                    break;
+                
+            }
+        });
+    }
+
+    let validInitials = function (initials){
+        let validInitials = false;
+        initials = initials.toUpperCase();
+        if(/^[A-Z]+$/.test(initials) && initials.length > 0){
+            validInitials = true;
+        }
+        else{
+            validInitials = false;
+        }
+
+        while (initials.length > 3 || initials.length < 1 || validInitials === false){
+            initials = prompt("Please enter at least one letter but no more than three");  
+            initials = initials.toUpperCase();
+        
+            if(/^[A-Z]+$/.test(initials)){
+                validInitials = true;
+            }
+            else{
+                validInitials = false;
+            }
+            
+        }
+
+        return initials;
+    }
+
+    
+
+    //This function is called when user wins the game
+
+    let youWon = function (){
+        let isHighScore = false;
+        qText.text("You Won!");
+        ansBtn.addClass("display");
+        result.addClass("display");
+        mainP.removeClass("display");
+        mainP.html("You answered " + numCorrect + " questions correctly!");
+
+        if (topScores.length > 0){
+            topScores.forEach(function(element){
+                if (timeCount > element.time){
+                    isHighScore = true;
+                }
+                return isHighScore;
+            });
+
+            if (isHighScore){
+                getInitials();
+            }
+            else{
+                mainP.html("You answered " + numCorrect + " questions correctly! <br> I'm sorry your time was not high enough to make the high score board.");
+            }
+        }
+        else{
+            getInitials();
+            
+        }   
+    }
+
+    //This function loops through highScores and places new object in the correct spot
+    let loopThroughScores = function(newObj){
+        for (let i = 0; i < topScores.length; i++){
+            if (newObj.time > topScores[i].time){
+                topScores.splice(i, 0, newObj);
+            }
+            break;
+        }
+    }
+
+    //This function creates an object
+    let scoreBoardEntry= function(initial, time){
+        this.initial = initial;
+        this.time = time;
+    }
+
+
+    //This function sets highScores
+
+    let setHighScore = function (initial, time){
+        let newObject = new scoreBoardEntry(initial, time);
+        switch(topScores.length){
+            case 0:
+                topScores.push(newObject);
+                break;
+            case 1:
+                if (newObject.time > topScores[0].time){
+                    topScores.splice(0, 0, newObject);
+                }
+                else{
+                    topScores.push(newObject);
+                }
+
+                break;
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                loopThroughScores(newObject);
+                break;
+        }
+    
+    }
+
     //This function starts the quiz and gets the first question;
     function startQuiz (){
         getQuestion();
@@ -168,52 +357,58 @@ $(document).ready(function() {
         startBtn.addClass("display");
         ansBtn.removeClass("display");
         
+        
     }
 
     //This event handler with anonymous function handles user answers
     $("button.quizBtn").on("click", function(){
-        $("form.answer-btns").css("border-bottom", "2px dashed lightgray");
+        ansBtn.css("border-bottom", "2px dashed lightgray");
         result.removeClass("display");
         
 
         if (this.value === answerKey){
             correctAns.play();
-            timerCount += 2;
+            timerCount += 5;
+            numCorrect ++;
             result.css("color", "green");   
             result.text("Correct!");
+            answeredQuestions++;
         }
         else {
             wrongAns.play();
             timerCount -= 15;
             result.css("color", "red");
-            result.text("Incorrect");  
+            result.text("Incorrect");
+            answeredQuestions++;  
         }
         
         getQuestion();
 
-        return false;
+        
     });
 
     //reset function 
     const resetFunc = function (){
         qText.text("Coding Quiz Challenge");
-        timerCount = 75;
+        timerCount = 60;
+        answerKey = "";
+        arrayLength = 12;
+        didWin = false;
+        numCorrect = 0;
+        answeredQuestions = 0;
+        initial = "";
         mainP.removeClass("display");
         startBtn.removeClass("display");
         ansBtn.addClass("display");
         result.addClass("display");
         highScores.addClass("display");
+        timeLeft.text(60)
 
         questionsUsed.forEach(function(element){
             quizQuestions.push(element);
         });
 
         questionsUsed.length = 0; 
-
-        //*****************test purposes *********************/\
-        console.log("quizQuestions: " + quizQuestions.length);
-        console.log("questionsUsed: " + questionsUsed.length);
-        //**************************** *************************/
     
     };
     
@@ -221,13 +416,10 @@ $(document).ready(function() {
     //Event listeners
     startBtn.on("click", startTime);
     startBtn.on("click", startQuiz);
+    backButton.on("click", resetFunc);
 
 });
 
-//**********************Testing purposes*************************
-// console.log("question: " + quizQuestions[5].question);
-// console.log("optionOne: " + quizQuestions[9].optionOne);
-// console.log("answer: " + quizQuestions[3].answer);
 
         
         
