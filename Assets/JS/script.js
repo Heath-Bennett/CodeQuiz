bfbff29ec21b30701ef63a0f6332082fab953b77
Heath Bennett <heathben418@gmail.com>
2021-01-23 08:13:49 -0500

    Fixed High Scores

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -159,15 +159,18 @@ $(document).ready(function() {
 
     //Retrieves High Scores
     let init = function (){
-        let storedScores = JSON.parse(localStorage.getItem("Top_Scores"));
-        if (storedScores !== null){
-            storedScores.forEach(function(e){
-                let storedIndex = 0;
-                topScores[storedIndex] = e;
-                console.log("Top Scores: " + topScores[storedIndex].initial + topScores[storedIndex].time);
-                storedIndex++
-            });
-        }
+        topScores = JSON.parse(localStorage.getItem("Top_Scores"));
+        // if (storedScores !== null){
+        //     storedScores.forEach(function(e){
+        //         console.log("init3", topScores);
+        //         let storedIndex = 0;
+        //         topScores[storedIndex] = e;
+        //         console.log("Top Scores: " + topScores[storedIndex].initial + topScores[storedIndex].time);
+        //         console.log("init2", topScores);
+        //         storedIndex++
+        //         console.log("init", topScores);
+        //     });
+        // }
     }
 
     init();
@@ -297,6 +300,7 @@ $(document).ready(function() {
     //This function determines if score is High Score
 
     let topScoreLength = function (time, array){
+        console.log("topScoreLength", topScores);
         switch(topScores.length){
             case 0:
             case 1:
@@ -338,6 +342,8 @@ $(document).ready(function() {
     let scoreBoardEntry= function(initial, time){
         this.initial = initial;
         this.time = time;
+        console.log("ScoreBoardEntery: ", topScores)
+        console.log('this: ', this);
     }
 
     //This function stores high scores
@@ -347,10 +353,12 @@ $(document).ready(function() {
     
     //This function sets highScores
     let setHighScore = function (initial, time){
+        console.log("setsHIghScore: ", topScores);
         let newObject = new scoreBoardEntry(initial, time);
+        console.log("newObject: ", newObject);
         topScores.push(newObject);
         topScores.sort((a, b) => b.time - a.time); 
-        // topScores.splice(5);
+        topScores.splice(5);
 
         topScores.forEach(function(e){
             console.log("Initials: " + e.initial + " Time: " + e.time);
